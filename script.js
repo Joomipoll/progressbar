53fbd267c02b04708b4cb8a18192440a8ea798c8
Dragan Đukanović <95223648+Joomipoll@users.noreply.github.com>
2023-07-31 15:29:55 +0700

    Improvement: Pass parameters by reference

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
+const progressBar = document.getElementById("progressbar");
+
+function changeStyles(params)
+{
+
+	if(params.percent)
+	{
+		progressBar.setAttribute('data-percent', new percent);
+	}
+
+	if(params.duration)
+	{
+		document.documentElement.style.setProperty('--duration', params.duration);
+	}
+	
+	if(params.bgclr)
+	{
+		document.documentElement.style.setProperty('--bgclr', params.bgclr);
+	}
+	
+	if(params.bgstart)
+	{
+		document.documentElement.style.setProperty('--bgstart', params.bgstart);
+	}
+	
+	if(params.bgfinish)
+	{
+		document.documentElement.style.setProperty('--bgfinish', params.bgfinish);
+	}
+}
+
 window.addEventListener("load", (event) => {
-    const progressBar = document.getElementById("progressbar");
-    const percentValue = progressBar.getAttribute("data-percent");
+	const percentValue = progressBar.getAttribute("data-percent");
+
+	var urlParams = new URLSearchParams(window.location.search);
+	var params = {};
+
+	urlParams.forEach(function(value, key) {
+		params[key] = value;
+	});
+
+	changeStyles(params);
+
     const valueWithoutPercent = parseFloat(percentValue.replace("%", ""));
     let interval = setInterval(() => {
     	const computedStyle = getComputedStyle(progressBar);
@@ -12,3 +52,4 @@ window.addEventListener("load", (event) => {
         }
     }, 5)
 });
+
